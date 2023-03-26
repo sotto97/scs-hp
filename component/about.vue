c76f6e1cce09dca546fb28bdc15cf0aac0312b55
sotto97 <sotto.bw97@gmail.com>
2023-03-26 17:02:52 +0900

    edit

--- component/about.vue
+++ component/about.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="p-2">
-		<h1 class="font-semibold text-xl"><span class="border-l-4 border-green-500 mr-2"></span>Creative集団 SCSとは？</h1>
+		<img src="../img/office.jpeg" alt="" class="rounded-lg shadow-lg" />
+		<!-- <h1 class="font-semibold text-xl mt-2 border-l-4 border-green-500 mr-2 pl-2">Sotto<br />Creative<br />Studioとは？</h1> -->
+		<h1 class="font-semibold text-xl mt-2 border-l-4 border-green-500 mr-2 pl-2">Sotto Creative Studioとは？</h1>
 		<p class="text-base">
 			シンプルに言えば「何でもやる」。「これができます」と言うんじゃなくて「こういうのできます？」を叶えたいですね。「Odd
 			Future」や「BROCKHAMPTON」などに影響を受けていて、福岡発のコレクティブとして進化していきたいと思っています。
---
+++ component/artist.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<div class="flex">
+			<div class="w-1/2">
+				<h1 class="font-semibold text-xl"><span class="border-l-4 border-green-500 mr-2"></span>所属アーティスト</h1>
+			</div>
+			<div class="w-1/2 text-right align-center">
+				<button class="text-black hover:text-white bg-white hover:bg-black transition border-2 border-solid py-1 px-6 text-xs">View Detail</button>
+			</div>
+		</div>
+
+		<div class="overflow-x-auto whitespace-nowrap pt-6">
+			<div class="inline-block h-auto w-6/12">
+				<div class="bg-black h-48 rounded-lg mr-2 shadow-xl">
+					<img src="../img/sotto.jpg" class="h-48 mx-auto rounded-lg" alt="" />
+				</div>
+				<p class="font-semibold pt-2 text-lg">Sotto<span class="pl-2 text-xs text-green-600">DJ/Engineer</span></p>
+			</div>
+			<div class="inline-block h-auto w-6/12">
+				<div class="bg-black h-48 rounded-lg mr-2 shadow-xl">
+					<img src="../img/sotto.jpg" class="h-48 mx-auto rounded-lg" alt="" />
+				</div>
+				<p class="font-semibold pt-2 text-lg">David<span class="pl-2 text-xs text-green-600">Photographer</span></p>
+			</div>
+			<div class="inline-block h-auto w-6/12">
+				<div class="bg-black h-48 rounded-lg mr-2 shadow-xl">
+					<img src="../img/sotto.jpg" class="h-48 mx-auto rounded-lg" alt="" />
+				</div>
+				<p class="font-semibold pt-2 text-lg">Atrax<span class="pl-2 text-xs text-green-600">Barista</span></p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+module.exports = {
+	data: function () {
+		return {
+			name: "ArtistComponent",
+		};
+	},
+};
+</script>
