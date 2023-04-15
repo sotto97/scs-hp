0577990bbfc49beea43b25c2d99d5af939c48f48
sotto97 <sotto.bw97@gmail.com>
2023-04-15 11:40:31 +0900

    画像のサイズ変更

--- component/top.vue
+++ component/top.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container flex justify-center items-center h-full">
-		<img src="../img/scs_logo.png" alt="image" class="mx-auto" />
+		<img src="../img/scs_logo_big_white.jpeg" alt="image" class="w-1/2 sm:w-1/3 md:w-1/4" />
 	</div>
 </template>
 <script>
