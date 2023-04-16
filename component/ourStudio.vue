1b68c1bc8c267b23e2217353191551292d705c0d
sotto97 <sotto.bw97@gmail.com>
2023-04-16 23:44:02 +0900

    サイドバーのカラーを変更

--- component/ourStudio.vue
+++ component/ourStudio.vue
@@ -8,64 +8,15 @@
 </template>
 
 <script>
-const routes = [
-	// { name: "foo", path: "/foo", component: httpVueLoader("./component/foo.vue") },
-	// { name: "bar", path: "/bar", component: httpVueLoader("./component/bar.vue") },
-	{ name: "music", path: "/music", component: httpVueLoader("./component/music.vue") },
-	{ name: "apparel", path: "/apparel", component: httpVueLoader("./component/apparel.vue") },
-	{ name: "about", path: "/about", component: httpVueLoader("./component/about.vue") },
-];
+// const routes = [
+// 	{ name: "music", path: "/music", component: httpVueLoader("./component/music.vue") },
+// 	{ name: "apparel", path: "/apparel", component: httpVueLoader("./component/apparel.vue") },
+// 	{ name: "about", path: "/about", component: httpVueLoader("./component/about.vue") },
+// ];
 
-const router = new VueRouter({
-	routes,
-});
-
-const nav_lists = [
-	{
-		name: "Our Studio",
-		icon: "mdi-garage-open",
-		path: "/about",
-	},
-	{
-		name: "Music",
-		params: "music",
-		title: "Music",
-		icon: "mdi-music",
-		lists: ["Event", "DJs"],
-		path: "/music",
-	},
-	{
-		name: "Apparel",
-		params: "apparel",
-		title: "Apparel",
-		icon: "mdi-tshirt-crew",
-		path: "/apparel",
-	},
-	{
-		name: "Coding",
-		params: "coding",
-		title: "Coding",
-		icon: "mdi-code-tags",
-		path: "",
-	},
-	{
-		name: "Cafe",
-		icon: "mdi-coffee",
-		lists: ["神戸", "代官山"],
-		path: "/apparel",
-	},
-	{
-		name: "Graphic",
-		icon: "mdi-camera-iris",
-		lists: ["Photographer"],
-		path: "",
-	},
-	{
-		name: "Fitness Gym",
-		icon: "mdi-dumbbell",
-		path: "",
-	},
-];
+// const router = new VueRouter({
+// 	routes,
+// });
 
 module.exports = {
 	data: function () {
--- component/top.vue
+++ component/top.vue
@@ -1,14 +1,12 @@
 <template>
 	<div class="container flex justify-center items-center h-full">
-		<img src="../img/scs_logo_big_white.jpeg" alt="image" class="w-1/2 sm:w-1/3 md:w-1/4" />
+		<img src="../img/scs_logo_big_white.jpeg" alt="" class="w-1/2 sm:w-1/3 md:w-1/4" />
 	</div>
 </template>
 <script>
 module.exports = {
 	data: function () {
-		return {
-			text: "Top",
-		};
+		return {};
 	},
 };
 </script>
