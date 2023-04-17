ddccb42f128b0be9692986551f55238dd4286d1a
sotto97 <sotto.bw97@gmail.com>
2023-04-17 23:41:47 +0900

    componentを適切に使用

--- component/ourStudio.vue
+++ component/ourStudio.vue
@@ -8,34 +8,18 @@
 </template>
 
 <script>
-// const routes = [
-// 	{ name: "music", path: "/music", component: httpVueLoader("./component/music.vue") },
-// 	{ name: "apparel", path: "/apparel", component: httpVueLoader("./component/apparel.vue") },
-// 	{ name: "about", path: "/about", component: httpVueLoader("./component/about.vue") },
-// ];
-
-// const router = new VueRouter({
-// 	routes,
-// });
+const AboutComponent = httpVueLoader("./about.vue");
+const ArtistComponent = httpVueLoader("./artist.vue");
+const NewsComponent = httpVueLoader("./news.vue");
+const QaComponent = httpVueLoader("./qa.vue");
 
 module.exports = {
-	data: function () {
-		return {
-			router: router,
-			components: {
-				AboutComponent: httpVueLoader("./component/about.vue"),
-				NewsComponent: httpVueLoader("./component/news.vue"),
-				QaComponent: httpVueLoader("./component/qa.vue"),
-				ArtistComponent: httpVueLoader("./component/artist.vue"),
-			},
-			vuetify: new Vuetify(),
-			data() {
-				return {
-					drawer: null,
-					nav_lists: nav_lists,
-				};
-			},
-		};
+	components: {
+		AboutComponent,
+		ArtistComponent,
+		NewsComponent,
+		QaComponent,
 	},
+	name: "OurStudioComponent",
 };
 </script>
--- component/top.vue
+++ component/top.vue
@@ -1,12 +1,15 @@
 <template>
 	<div class="container flex justify-center items-center h-full">
+		;;;
 		<img src="../img/scs_logo_big_white.jpeg" alt="" class="w-1/2 sm:w-1/3 md:w-1/4" />
 	</div>
 </template>
 <script>
 module.exports = {
 	data: function () {
-		return {};
+		return {
+			name: "topComponent",
+		};
 	},
 };
 </script>
