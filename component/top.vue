876ff6c911fb113406f471fdd2ef746fa37d9efb
sotto97 <sotto.bw97@gmail.com>
2023-04-17 23:42:47 +0900

    不要文字の除外

--- component/top.vue
+++ component/top.vue
@@ -1,6 +1,5 @@
 <template>
 	<div class="container flex justify-center items-center h-full">
-		;;;
 		<img src="../img/scs_logo_big_white.jpeg" alt="" class="w-1/2 sm:w-1/3 md:w-1/4" />
 	</div>
 </template>
