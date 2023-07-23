260330e0f83812a3e0c2e2f969d5f0bd4f31203c
sotto97 <sotto.bw97@gmail.com>
2023-07-23 20:24:36 +0900

    Youtubeリンクに差し替え

---
+++ component/music/music_bk.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container flex justify-center items-center h-full">
+		<iframe
+			width="80%"
+			height="80%"
+			src="https://www.youtube.com/embed/5v-z7jKJbxc"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+		></iframe>
+	</div>
+</template>
+<script>
+module.exports = {
+	data: function () {
+		return {
+			text: "Music",
+		};
+	},
+};
+</script>
