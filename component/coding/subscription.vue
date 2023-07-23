0fa06e534b8005db80e0b4a93304b94a3eec915a
sotto97 <sotto.bw97@gmail.com>
2023-07-23 20:18:27 +0900

    Youtubeリンクに差し替え

--- component/coding/subscription.vue
+++ component/coding/subscription.vue
@@ -11,7 +11,7 @@
 			</div> -->
 			<div class="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8">
 				<!-- article - start -->
-				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row bg-indigo-100">
 					<a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
 						<iframe
 							width="100%"
@@ -23,86 +23,88 @@
 							allowfullscreen
 						></iframe>
 					</a>
-
-					<div class="flex flex-col gap-2 p-4 lg:p-6">
-						<h2 class="text-xl font-bold text-gray-800">
-							<a href="#" class="">
-								<span class="text-indigo-500 truncate">日本語ラップとか個人的にエモくなれる曲 Mix</span>
-							</a>
+					<div class="flex flex-col gap-2 p-4 lg:p-6 w-32">
+						<h2 class="text-xl font-bold text-gray-800 truncate">
+							<span class="text-indigo-500 truncate">日本語ラップとか個人的にエモくなれる曲 Mix</span>
 						</h2>
-						<span class="text-sm text-gray-400">1,994,617 回視聴 2020/11/15</span>
-						<p class="text-gray-500 overflow-ellipsis overflow-hidden h-12">
-							僕のSpotifyのプレイリストから選んで作ってみました。 垂れ流しじゃなく、Mashupしてみました。 良いと思ってもらえれば幸いです。 よろしくどうぞ。 おすすめの曲とか、こんな曲良いよ〜とかあれば
-							コメントで教えてください。 ※利益は著作権主に流れます。僕自身に収益が入ってくることはありません。
-						</p>
+						<span class="text-sm text-gray-400">1,994,617 回視聴</span>
+						<span class="text-sm text-gray-400">公開日：2020/11/15</span>
+						<!-- <p class="text-gray-500 overflow-ellipsis overflow-hidden h-12 truncate">
+							僕のSpotifyのプレイリストから選んで作ってみました。 垂れ流しじゃなく、Mashupしてみました。 良いと思ってもらえれば幸いです。 よろしくどうぞ。
+							おすすめの曲とか、こんな曲良いよ〜とかあればコメントで教えてください。 ※利益は著作権主に流れます。僕自身に収益が入ってくることはありません。
+						</p> -->
 					</div>
 				</div>
-				<!-- article - end -->
-
-				<!-- article - start -->
-				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row bg-indigo-100">
 					<a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
-						<img
-							src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600"
-							loading="lazy"
-							alt="Photo by Lorenzo Herrera"
-							class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-						/>
+						<iframe
+							width="100%"
+							height="100%"
+							src="https://www.youtube.com/embed/XCSweAYe920"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							allowfullscreen
+						></iframe>
 					</a>
-					<div class="flex flex-col gap-2 p-4 lg:p-6">
-						<h2 class="text-xl font-bold text-gray-800">
-							<a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">
-								<span class="text-indigo-500">Youtube</span>
-							</a>
+					<div class="flex flex-col gap-2 p-4 lg:p-6 w-32">
+						<h2 class="text-xl font-bold text-gray-800 truncate">
+							<span class="text-indigo-500 truncate">友達が夜に聴いているらしい曲 Mix</span>
 						</h2>
-						<span class="text-sm text-gray-400">April 07, 2021</span>
-						<p class="text-gray-500">This is a section of some simple filler text, also known as placeholder text.</p>
+						<span class="text-sm text-gray-400">2,004,924 回視聴</span>
+						<span class="text-sm text-gray-400">公開日：2020/12/12</span>
+						<!-- <p class="text-gray-500 overflow-ellipsis overflow-hidden h-12 truncate">
+							僕のSpotifyのプレイリストから選んで作ってみました。 垂れ流しじゃなく、Mashupしてみました。 良いと思ってもらえれば幸いです。 よろしくどうぞ。
+							おすすめの曲とか、こんな曲良いよ〜とかあればコメントで教えてください。 ※利益は著作権主に流れます。僕自身に収益が入ってくることはありません。
+						</p> -->
 					</div>
 				</div>
-				<!-- article - end -->
-
-				<!-- article - start -->
-				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row bg-indigo-100">
 					<a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
-						<img
-							src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=600"
-							loading="lazy"
-							alt="Photo by Magicle"
-							class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-						/>
+						<iframe
+							width="100%"
+							height="100%"
+							src="https://www.youtube.com/embed/Nifb4yEXwTg"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							allowfullscreen
+						></iframe>
 					</a>
-
-					<div class="flex flex-col gap-2 p-4 lg:p-6">
-						<h2 class="text-xl font-bold text-gray-800">
-							<a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">
-								<span class="text-indigo-500">AmazonPrime</span>
-							</a>
+					<div class="flex flex-col gap-2 p-4 lg:p-6 w-32">
+						<h2 class="text-xl font-bold text-gray-800 truncate">
+							<span class="text-indigo-500 truncate">一日の終わりに聴くと良さそうな曲 Mix</span>
 						</h2>
-						<span class="text-sm text-gray-400">March 15, 2021</span>
-						<p class="text-gray-500">This is a section of some simple filler text, also known as placeholder text.</p>
+						<span class="text-sm text-gray-400">1,904,021 回視聴</span>
+						<span class="text-sm text-gray-400">公開日：2021/01/19</span>
+						<!-- <p class="text-gray-500 overflow-ellipsis overflow-hidden h-12 truncate">
+							僕のSpotifyのプレイリストから選んで作ってみました。 垂れ流しじゃなく、Mashupしてみました。 良いと思ってもらえれば幸いです。 よろしくどうぞ。
+							おすすめの曲とか、こんな曲良いよ〜とかあればコメントで教えてください。 ※利益は著作権主に流れます。僕自身に収益が入ってくることはありません。
+						</p> -->
 					</div>
 				</div>
-				<!-- article - end -->
-
-				<!-- article - start -->
-				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+				<div class="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row bg-indigo-100">
 					<a href="#" class="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
-						<img
-							src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600"
-							loading="lazy"
-							alt="Photo by Martin Sanchez"
-							class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-						/>
+						<iframe
+							width="100%"
+							height="100%"
+							src="https://www.youtube.com/embed/fFBKntE_AP0"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							allowfullscreen
+						></iframe>
 					</a>
-
-					<div class="flex flex-col gap-2 p-4 lg:p-6">
-						<h2 class="text-xl font-bold text-gray-800">
-							<a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">
-								<span class="text-indigo-800">U-next</span>
-							</a>
+					<div class="flex flex-col gap-2 p-4 lg:p-6 w-32">
+						<h2 class="text-xl font-bold text-gray-800 truncate">
+							<span class="text-indigo-500 truncate">とりあえず夜に部屋で流しておく曲 Mix</span>
 						</h2>
-						<span class="text-sm text-gray-400">January 27, 2021</span>
-						<p class="text-gray-500">This is a section of some simple filler text, also known as placeholder text.</p>
+						<span class="text-sm text-gray-400">694,111 回視聴</span>
+						<span class="text-sm text-gray-400">公開日：2022/09/19</span>
+						<!-- <p class="text-gray-500 overflow-ellipsis overflow-hidden h-12 truncate">
+							僕のSpotifyのプレイリストから選んで作ってみました。 垂れ流しじゃなく、Mashupしてみました。 良いと思ってもらえれば幸いです。 よろしくどうぞ。
+							おすすめの曲とか、こんな曲良いよ〜とかあればコメントで教えてください。 ※利益は著作権主に流れます。僕自身に収益が入ってくることはありません。
+						</p> -->
 					</div>
 				</div>
 				<!-- article - end -->
