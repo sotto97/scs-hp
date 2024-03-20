268d1392667ad94548b058ba370838c5bff53554
sotto97 <sotto.bw97@gmail.com>
2024-03-20 10:30:10 +0900

    Refactor form layout in qa.vue

--- component/ourStudio/qa.vue
+++ component/ourStudio/qa.vue
@@ -6,35 +6,37 @@
 				<h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">問い合わせ</h2>
 
 				<form @submit.prevent="submitForm">
-					<!-- Name -->
-					<div class="sm:col-span-4">
-						<label for="username" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-						<div class="mt-2">
-							<div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-								<input
-									type="text"
-									name="username"
-									id="username"
-									autocomplete="username"
-									class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-									placeholder="janesmith"
-								/>
+					<div class="block md:flex">
+						<!-- Name -->
+						<div class="w-full md:w-1/2">
+							<label for="username" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+							<div class="mt-2">
+								<div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+									<input
+										type="text"
+										name="username"
+										id="username"
+										autocomplete="username"
+										class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+										placeholder="janesmith"
+									/>
+								</div>
 							</div>
 						</div>
-					</div>
-					<!-- Email -->
-					<div class="sm:col-span-4">
-						<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-						<div class="mt-2">
-							<div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-								<input
-									type="text"
-									name="email"
-									id="email"
-									autocomplete="email"
-									class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-									placeholder="scs@example.com"
-								/>
+						<!-- Email -->
+						<div class="w-full md:w-1/2">
+							<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+							<div class="mt-2">
+								<div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+									<input
+										type="text"
+										name="email"
+										id="email"
+										autocomplete="email"
+										class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+										placeholder="scs@example.com"
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
