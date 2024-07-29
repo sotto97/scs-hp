dc1512781c01b50014600773abc44f64bcd3f9ee
sotto97 <sotto.bw97@gmail.com>
2024-07-29 23:02:05 +0900

    Update about page content

--- component/ourStudio/about.vue
+++ component/ourStudio/about.vue
@@ -3,7 +3,6 @@
 		<div class="bg-white py-6 sm:py-8 lg:py-12">
 			<div class="mx-auto max-w-screen-2xl px-4 md:px-8">
 				<p class="mb-2 text-center font-semibold text-emerald-500 md:mb-3 lg:text-lg">Mission</p>
-				<!-- <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">SCSとは</h2> -->
 				<p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
 					Our mission is to maximize our clients' growth potential. By embracing new challenges and continually evolving, we aim to shape the future together.
 				</p>
@@ -12,10 +11,8 @@
 		<div class="bg-white py-6 sm:py-8 lg:py-12">
 			<div class="mt-4 mx-auto max-w-screen-2xl px-4 md:px-8">
 				<p class="mb-2 text-center font-semibold text-emerald-500 md:mb-3 lg:text-lg">Vision</p>
-				<!-- <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">SCSの構成は？</h2> -->
 				<p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
-					Our vision is to create a society where everyone believes in their growth potential and fully utilizes it. We leverage new technologies and knowledge to be partners in realizing our clients'
-					dreams.
+					Create a society where everyone believes in their growth potential and fully utilizes it. We leverage new technologies and knowledge to be partners in realizing our clients
 				</p>
 			</div>
 		</div>
